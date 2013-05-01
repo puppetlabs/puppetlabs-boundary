533c1bdda181d4c9fc70c6dacf73a2157113082b
Valentino Tereshko <valentino@boundary.com>
2013-05-01 13:17:04 -0700

    Added new boundary.rb file.

--- lib/puppet/reports/boundary.rb
+++ lib/puppet/reports/boundary.rb
@@ -23,13 +23,17 @@ Puppet::Reports.register_report(:boundary) do
       return if self.status == 'unchanged'
       if self.status == 'failed'
         tags = ["puppet", "exception", "failure", self.status ]
-        type = "Puppet Exception"
+        title = "Puppet Exception"
+        status = "OPEN"
+        severity = "ERROR"
       else
         tags = ["puppet", self.status ]
-        type = "Puppet"
+        title = "Puppet"
+        status = "OK"
+        severity = "INFO"
       end
 
-      create_annotation(tags,type,self.host,self.time)
+      create_event(title, tags,status, severity ,self.host,self.time)
     end
   else
     Puppet.debug "Boundary annotations disabled"
@@ -38,45 +42,49 @@ Puppet::Reports.register_report(:boundary) do
     end
   end
 
-  def create_annotation(tags,type,host,time)
+  def create_event( title, tags, status, severity, host,time)
     auth = auth_encode("#{BOUNDARY_API}:")
     headers = {"Authorization" => "Basic #{auth}", "Content-Type" => "application/json"}
+    fingerprints=["@title"]
+    source = {"ref" => host, "type" => "meter" }
+    event = {
+	 :source => source,
+      :severity => severity,
+      :status => status,
+      :fingerprintFields => fingerprints,
+      :tags => tags,
+      :title => title ,
+      :message => host,
+      :createdAt => time.to_i
 
-    annotation = {
-      :type => type,
-      :subtype => host,
-      :start_time => time.to_i,
-      :end_time => time.to_i,
-      :tags => tags
     }
 
-    annotation_pson = annotation.to_pson
-
-    uri = URI("https://api.boundary.com/#{BOUNDARY_ORG}/annotations")
+    event_json = event.to_json
+    uri = URI("https://api.boundary.com/events/#{BOUNDARY_ORG}")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    http.ca_file = "#{File.dirname(__FILE__)}/cacert.pem"
+    http.ca_file = "#{File.dirname(__FILE__)}/cacert.pem" 
     http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
     begin
       timeout(10) do
         req = Net::HTTP::Post.new(uri.request_uri)
-        req.body = annotation_pson
+        req.body = event_json
 
-        headers.each{|k,v|
-          req[k] = v
+        headers.each{|k,v| 
+          req[k] = v 
         }
 
         res = http.request(req)
 
         bad_response?(:post, uri.request_uri, res)
 
-        Puppet.info "Created a Boundary Annotation @ #{res["location"]}"
-        res["location"]
+        Chef::Log.info("Created a Boundary Event @ #{res["location"]}")
+        res["location"]  
       end
     rescue Timeout::Error
-      Puppet.error "Timed out while attempting to create Boundary Annotation"
-    end
+      Chef::Log.error("Timed out while attempting to create Boundary Event")
+      end
   end
 
   def auth_encode(creds)
